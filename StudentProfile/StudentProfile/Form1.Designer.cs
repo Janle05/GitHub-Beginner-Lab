31a9c3c8b60022f4656705054c1485e226af14ac
Janle05 <jhonleedulc6@gmail.com>
2026-09-14 19:58:39 +0800

    Add student contact number

--- StudentProfile/StudentProfile/Form1.Designer.cs
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -35,11 +35,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(157, 196);
+            this.label1.Location = new System.Drawing.Point(122, 201);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(475, 37);
+            this.label1.Size = new System.Drawing.Size(553, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Student Profile - Jhon Lee Dulce";
+            this.label1.Text = "Student Profile - GitHub Beginner Lab";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
--- StudentProfile/StudentProfile/Form1.cs
+++ StudentProfile/StudentProfile/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,7 +16,7 @@ namespace StudentProfile
         public Form1()
         {
             InitializeComponent();
-            label1.Text = "Student Profile — GitHub Beginner Lab";
+            label1.Text = "Student Profile — GitHub Beginner Lab\nContact Number: 09171234567";
         }
 
         private void label1_Click(object sender, EventArgs e)
